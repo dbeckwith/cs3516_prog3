96d072f6b08a4f0223b2b3ad6f44347bfd757628
Daniel Beckwith <djbsnx@gmail.com>
2014-12-15 21:34:45 -0500

    Added sequence numbers and frame timeouts.

--- client_data_link_layer.c
+++ client_data_link_layer.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <errno.h>
 #include "util.h"
 #include "network_layer.h"
 #include "client_network_layer.h"
@@ -24,6 +26,7 @@ int data_link_send_packet(int socket, packet_t* packet)
     size_t packet_size;
     unsigned int pos;
     unsigned int chunk_len;
+	static seq_t curr_seq_num = 0;
 
     packet_size = sizeof(packet_t);
     frame_count = 0;
@@ -41,26 +44,46 @@ int data_link_send_packet(int socket, packet_t* packet)
         // copy packet bytes into frame data
         memcpy(frame.frame.data, packet->bytes + pos, chunk_len);
         frame.frame.data_length = chunk_len;
+        memcpy(&frame.frame.seq_num, &curr_seq_num, sizeof(curr_seq_num));
 
-        // send frame through physical layer
-        if (DEBUG) printf(DATA_LINK_STR "sending frame through physical layer\n");
-        if (physical_send_frame(socket, &frame) != sizeof(frame_t))
-        {
-            return -1;
-        }
+        while (true) {
+	        // send frame through physical layer
+	        if (DEBUG) printf(DATA_LINK_STR "sending frame through physical layer\n");
+	        if (physical_send_frame(socket, &frame) != sizeof(frame_t))
+	        {
+	            return -1;
+	        }
 
-        photo_log(socket, "Frame %d of packet %d sent successfully.\n", ++frame_count, packet_count);
+	        photo_log(socket, "Frame %d of packet %d sent successfully.\n", ++frame_count, packet_count);
 
-        // wait for ACK frame
-        if (DEBUG) printf(DATA_LINK_STR "waiting for frame ack through physical layer\n");
-        if (physical_recv_frame(socket, &frame) != sizeof(frame_t)) {
-            if (DEBUG) printf(DATA_LINK_STR "frame ack was wrong size\n");
-            return -1;
-        }
-        if (!IS_ACK_FRAME(frame.frame)) {
-            if (DEBUG) printf(DATA_LINK_STR "frame ack was not an ack\n");
-            return -1;
-        }
+	        // wait for ACK frame
+	        if (DEBUG) printf(DATA_LINK_STR "waiting for frame ack through physical layer\n");
+	        if (physical_recv_frame(socket, &frame) != sizeof(frame_t)) {
+	            if (DEBUG) printf(DATA_LINK_STR "error receiving frame ack\n");
+	            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+	            	if (DEBUG) printf(DATA_LINK_STR "timeout waiting for ack frame");
+	            	// timeout
+	            	continue;
+	            }
+	            return -1;
+	        }
+
+	        if (!IS_ACK_FRAME(frame.frame)) {
+	            if (DEBUG) printf(DATA_LINK_STR "frame ack was not an ack\n");
+	            return -1;
+	        }
+
+	        // check ACK's seq num
+	        if (frame.frame.seq_num != curr_seq_num) {
+            	if (DEBUG) printf(DATA_LINK_STR "ack frame was wrong sequence number");
+	        	continue;
+	        }
+
+	        if (DEBUG) printf(DATA_LINK_STR "ack frame accepted\n");
+	        break;
+	    }
+
+        INC_SEQ(curr_seq_num);
     }
     return sizeof(packet_t);
 }
--- data_link_layer.h
+++ data_link_layer.h
@@ -6,12 +6,16 @@
 #define DATA_LINK_STR "[DATA LINK LAYER]: " // String to display for debugging at data link layer
 #define FRAME_DATA_SIZE 124
 
+#define SEQ_LEN 2
+#define INC_SEQ(seq) ((seq) = ((seq) + 1) % SEQ_LEN)
+typedef uint16_t seq_t;
+
 // Union definition for frame for easy conversion to bytes to send
 typedef union
 {
     struct frame
     {
-        uint8_t seq_num[2];
+        seq_t seq_num;
         uint8_t data[FRAME_DATA_SIZE];
         uint8_t data_length;
         uint8_t error_detect[2];
